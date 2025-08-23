662049e396d79b042bf84895f8c7ae85bbbfde81
Pedro Henrique Quitete Barreto <60863081+pedrohqb@users.noreply.github.com>
2025-08-23 20:56:49 -0300

    Add files via upload

---
+++ instalar-dotfiles.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Define o URL do repositório
+REPO_URL="https://github.com/pedrohqb/dotfiles.git"
+TEMP_DIR="/tmp/dotfiles_temp"
+
+# ---
+
+# 1. Cria um diretório temporário e clona o repositório
+echo "Criando diretório temporário e clonando o repositório..."
+mkdir -p "$TEMP_DIR"
+git clone --depth 1 "$REPO_URL" "$TEMP_DIR"
+
+if [ $? -ne 0 ]; then
+    echo "Erro: Não foi possível clonar o repositório. Abortando."
+    rm -rf "$TEMP_DIR"
+    exit 1
+fi
+
+# ---
+
+# 2. Copia os arquivos para seus respectivos diretórios
+echo "Copiando arquivos de configuração..."
+
+# Diretório para os arquivos de fonte
+FONT_CONFIG_DIR="$HOME/.config/fontconfig/conf.d"
+echo "Verificando e criando diretório: $FONT_CONFIG_DIR"
+mkdir -p "$FONT_CONFIG_DIR"
+cp "$TEMP_DIR/config/fontconfig/conf.d/30-0-google-crosextra-caladea-fontconfig.conf" "$FONT_CONFIG_DIR/"
+cp "$TEMP_DIR/config/fontconfig/conf.d/30-0-google-crosextra-carlito-fontconfig.conf" "$FONT_CONFIG_DIR/"
+
+# Diretório para o plugin do Evolution
+EVOLUTION_PLUGIN_DIR="$HOME/.var/app/org.gnome.Evolution/data/evolution/webkit-editor-plugins"
+echo "Verificando e criando diretório: $EVOLUTION_PLUGIN_DIR"
+mkdir -p "$EVOLUTION_PLUGIN_DIR"
+cp "$TEMP_DIR/var/app/org.gnome.Evolution/data/evolution/webkit-editor-plugins/body-font.js" "$EVOLUTION_PLUGIN_DIR/"
+
+# ---
+
+# 3. Limpa o diretório temporário
+echo "Limpando arquivos temporários..."
+rm -rf "$TEMP_DIR"
+
+# ---
+
+echo "Instalação dos dotfiles concluída! O diretório temporário foi removido."
